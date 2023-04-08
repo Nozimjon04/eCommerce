c7c4ee06b2f71262e8d8719836cf563f70c5206e
Nozimjon Usmonaliyev <usminaliyevnozimjon@gmail.com>
2023-04-08 17:52:54 +0500

    Completed the repositoreis

---
+++ eCommerce.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using eCommerce.Domain.Entities;
+using eCommerce.Domain.Entities.Carts;
+using eCommerce.Domain.Entities.Orders;
+using eCommerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace eCommerce.Data.DbContexts;
+
+public class AppDbContext:DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+	{
+
+	}
+	DbSet<User> Users { get; set; }
+	DbSet<Payment> Payments { get; set; }	
+	DbSet<Product> Products { get; set;}
+	DbSet<Order> Orders { get;set; }
+	DbSet<ProductComment> ProductComments { get; set; }
+	DbSet<Cart> Carts { get; set; }
+	DbSet<CartProduct> CartProducts { get; set; }
+	DbSet<OrderComments> OrderComments { get; set; }
+
+
+}
--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -1,8 +1,12 @@
+using eCommerce.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("eCommerce.Web")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
