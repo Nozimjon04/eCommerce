5244027ec35e96a3c69882ea2371916759ceef7b
Nozimjon Usmonaliyev <usminaliyevnozimjon@gmail.com>
2023-04-08 18:18:59 +0500

    added migration

--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("eCommerce.Web")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("eCommerce")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
