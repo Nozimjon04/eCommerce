5b28f6f4c2700580518b6ab01753a58395a7f287
Muxammad <swe.muhammad@gmail.com>
2023-04-08 13:06:47 +0500

    Entities updated and added

--- eCommerce.Domain/Entities/Products/ProductComment.cs
+++ eCommerce.Domain/Entities/Products/ProductComment.cs
@@ -7,7 +7,7 @@ public class ProductComment : Auditable
     public long UserId { get; set; }
     public User User { get; set; } = default!;
     public long ProductId { get; set; }
-    public Product Prdduct { get; set; }
+    public Product Product { get; set; } = default!;
 
     public string Comment { get; set; } = string.Empty;
 }
