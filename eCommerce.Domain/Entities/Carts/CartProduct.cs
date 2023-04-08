8f3d1952c802e5291f40da1659ebe23982c65a20
Muxammad <swe.muhammad@gmail.com>
2023-04-08 12:43:32 +0500

    Entities Added

---
+++ eCommerce.Domain/Entities/Carts/CartProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Domain.Entities.Carts
+{
+    internal class CartProduct
+    {
+    }
+}
