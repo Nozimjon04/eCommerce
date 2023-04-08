2f94b84fc42db90d4b5a57bdf823f26c37377423
Nozimjon Usmonaliyev <usminaliyevnozimjon@gmail.com>
2023-04-09 00:02:15 +0500

    interface classes and auto mappers have been done

--- eCommerce.Service/Interfaces/IProductCommentService.cs
+++ eCommerce.Service/Interfaces/IProductCommentService.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace eCommerce.Service.Interfaces;
 
-namespace eCommerce.Service.Interfaces
+public interface IProductCommentService
 {
-	internal class IProductCommentService
-	{
-	}
 }
--- eCommerce.Service/Mappers/MappingProfile.cs
+++ eCommerce.Service/Mappers/MappingProfile.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
 
-namespace eCommerce.Service.Mappers
+namespace eCommerce.Service.Mappers;
+
+public class MappingProfile:Profile
 {
-	internal class MappingProfile
+	public MappingProfile()
 	{
+
 	}
 }
