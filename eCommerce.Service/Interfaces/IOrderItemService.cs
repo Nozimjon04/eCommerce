990810c24e7cc82cc55a749a323e9575e75b2173
Nozimjon Usmonaliyev <usminaliyevnozimjon@gmail.com>
2023-04-09 14:33:29 +0500

    making correction

--- eCommerce.Service/Interfaces/IOrderItemService.cs
+++ eCommerce.Service/Interfaces/IOrderItemService.cs
@@ -1,5 +1,15 @@
-﻿namespace eCommerce.Service.Interfaces;
+﻿using eCommerce.Domain.Entities;
+using eCommerce.Domain.Entities.Orders;
+using eCommerce.Service.DTOs.Orders;
+using System.Linq.Expressions;
 
-public class IOrderItemService
+namespace eCommerce.Service.Interfaces;
+
+public interface IOrderItemService
 {
+	public Task<Order> AddAsync(OrderItemCreationDto orderItemCreationDto);
+	public Task<Order> UpdateAsync(Expression<Func<OrderItemCreationDto, bool>> expression);
+	public Task<bool> DelateAsync(Expression<Func<OrderItemCreationDto>> expression);
+	public Task<Order> GetAsync(Expression<Func<OrderItemCreationDto>> expression);
+	public Task<IEnumerable<Order>> GetAllAsync(Expression<Func<OrderItem>> expression);
 }
