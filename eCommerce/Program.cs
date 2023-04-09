f85c8a8f793b2ebe741e102cfc58549c12cc4bd5
Nozimjon Usmonaliyev <usminaliyevnozimjon@gmail.com>
2023-04-09 10:42:37 +0500

    Repositores have been injected

--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -2,15 +2,33 @@ using eCommerce.Data.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
 using eCommerce.Service.Mappers;
+using eCommerce.Data.IRepositories;
+using eCommerce.Domain.Entities;
+using eCommerce.Data.Repositories;
+using eCommerce.Domain.Entities.Products;
+using eCommerce.Domain.Entities.Orders;
+using eCommerce.Domain.Entities.Carts;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region repositories added
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("eCommerce")));
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddScoped<IRepository<User>,Repository<User>>();
+builder.Services.AddScoped<IRepository<Product>,Repository<Product>>();
+builder.Services.AddScoped < IRepository < ProductComment >, Repository < ProductComment >> ();
+builder.Services.AddScoped<IRepository<Payment>, Repository<Payment>> ();
+builder.Services.AddScoped<IRepository<Order>, Repository<Order>> ();
+builder.Services.AddScoped<IRepository<OrderComments>, Repository<OrderComments>>();
+builder.Services.AddScoped<IRepository<Cart>,Repository<Cart>> ();
+builder.Services.AddScoped<IRepository<CartProduct>,Repository<CartProduct>> ();
 var app = builder.Build();
+
+#endregion
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
