0b3573560c77e03596003e0d9f38a07c19d7ee54
Muxammad <swe.muhammad@gmail.com>
2023-04-10 11:42:41 +0500

    Modified and changed

--- eCommerce.Service/Interfaces/IProductService.cs
+++ eCommerce.Service/Interfaces/IProductService.cs
@@ -10,9 +10,7 @@ public interface IProductService
 	public Task<productForResultDto> UpdateAsync(Expression<Func<Product, bool>> expression,ProductCreationDto dto);
 	public Task<bool> DelateAsync(Expression<Func<Product, bool>> expression);
 	public Task<productForResultDto> GetAsync(Expression<Func<Product,bool>> expression);
-<<<<<<< HEAD
+
 	public Task<IEnumerable<productForResultDto>> GetAllAsync();
-=======
-	public Task<IEnumerable<Product>> GetAllAsync(Expression<Func<Product,bool>> expression=null);
->>>>>>> 8eac051cc502ee52ec0bf15ce6223c99f0732ff5
+
 }
